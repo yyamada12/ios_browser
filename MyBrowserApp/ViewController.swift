a2369c38dbf596150f73e9e42352afc70cc89fc3
yyamada12 <12yacropolisy@gmail.com>
2019-05-19 15:09:08 +0900

    [add] interface bundle

--- MyBrowserApp/ViewController.swift
+++ MyBrowserApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var browserWebView: UIWebView!
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goBack(_ sender: Any) {
+    }
+    @IBAction func goForward(_ sender: Any) {
+    }
+    @IBAction func reload(_ sender: Any) {
+    }
+    
 }
 
